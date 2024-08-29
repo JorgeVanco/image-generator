870248939495de4ca7df3660ec64e1c1331a6220
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2024-08-29 09:03:48 +0200

    Removed torch transforms

--- src/data_processing/Dataset.py
+++ src/data_processing/Dataset.py
@@ -1,16 +1,12 @@
 from torch._tensor import Tensor
 from torch.utils.data import Dataset
 from utils import read_npy
-import torchvision.transforms as transforms
 
 
 class PixelDataset(Dataset):
     def __init__(self, sprites_path, labels_path) -> None:
         self.sprites = read_npy(sprites_path)
         self.labels = read_npy(labels_path)
-        # self.transform = transforms.Lambda(
-        #     lambda x: x / 255.0
-        # )  # Normalize to range [0, 1]
 
     def __len__(self) -> int:
         return len(self.sprites)
